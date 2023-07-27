9fc9aa4c0ba40df537d54e65d5aae980beaba388
danny-yamamoto <yamamoto_daisuke@retail-ai.jp>
2023-07-27 10:30:10 +0000

    Change users from internal to graph.

--- users/server.go
+++ users/server.go
@@ -8,17 +8,19 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/danny-yamamoto/go-graphql-federation-example/users/graph"
+	"github.com/danny-yamamoto/go-graphql-federation-example/users/internal"
 )
 
 const defaultPort = "4000"
 
 func main() {
-	port := os.Getenv("PORT")
+	port := os.Getenv("USERS_PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	es := internal.NewExecutableSchema(internal.Config{Resolvers: &graph.Resolver{}})
+	srv := handler.NewDefaultServer(internal.NewExecutableSchema(internal.Config{Resolvers: &graph.Resolver{Schema: es.Schema()}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
